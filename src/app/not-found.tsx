5ce27d686a130c8f77bc093238bd833f2410ea4d
ivankprod <houseprotector@mail.ru>
2024-06-27 13:20:02 +0500

    chore: base project template, close #3 issue

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import styles from "./not-found.module.css";
+
+function NotFoundPage() {
+	return (
+		<div className={styles.className}>
+			<h2>Ошибка 404: Не найдено</h2>
+		</div>
+	);
+}
+
+export default NotFoundPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+	return <main className={styles.main}>Homepage</main>;
+}
+
