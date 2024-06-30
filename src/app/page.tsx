e0558aed841cd351c253005817630c3b030b167f
ivankprod <houseprotector@mail.ru>
2024-06-30 13:41:13 +0500

    feat(ui-kit): LoginGoogleButton component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Caption from "@/lib/ui/components/Caption";
-
 import styles from "./page.module.css";
 
 export default function Home() {
-	return <main className={styles.main}><Caption text="New caption" /></main>;
+	return <main className={styles.main}></main>;
 }
