042acb6ff64be1b9edc01c0fed74ee59327d8e14
Denis <denis.shagim@icloud.com>
2024-07-17 02:35:38 +0500

    feat: create contact and add style for this section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,34 +1,53 @@
-import React from "react";
 import styles from "./Contact.module.css";
+
+import callIcon from "@/images/icons/phone-contact.svg";
+import mailIcon from "@/images/icons/mail-contact.svg";
+import Image from "next/image";
 
 export default function Contact() {
 	return (
-		<div className={styles.contactContainer}>
-			<header />
-			<div className={styles.contactContent}>
-				<h2>Contact</h2>
-				<div className={styles.contactInfo}>
+		<main className={styles.contactContainer}>
+			<section style={{
+				display: "flex",
+				margin: "80px 0 140px"
+			}}>
+				<aside style={{
+					borderRadius: 4,
+					boxShadow: "0px 1px 13px 0px rgba(0, 0, 0, 0.05)",
+					background: "rgb(255, 255, 255)",
+					padding: "40px 35px",
+					width: 340
+				}}>
 					<div className={styles.box}>
-						<h3>Call To Us</h3>
-						<p>We are available 24/7, 7 days a week.</p>
+						<h3>
+							<Image src={callIcon} alt="Call" />
+							Call To Us</h3>
+						<p style={{
+							marginTop: 24
+						}}>We are available 24/7, 7 days a week.</p>
 						<p>Phone: +88015-88888-9999</p>
+						<span></span>
+						<h3>
+							<Image src={mailIcon} alt="Mail" />
+							Write To Us</h3>
+						<p style={{
+							marginTop: 24
+						}}>Fill out our form and we will contact you within 24 hours.</p>
+						<p>Email: <a
+							href="mailto:customer@exclusive.com">customer@exclusive.com</a></p>
+						<p>Email: <a
+							href="mailto:support@exclusive.com">support@exclusive.com</a></p>
 					</div>
-					<div className={styles.box}>
-						<h3>Write To Us</h3>
-						<p>Email: <a href="mailto:contact@exclusive.com">contact@exclusive.com</a></p>
-						<p>We'll respond within 24 hours</p>
-					</div>
-				</div>
+				</aside>
 				<form className={styles.contactForm}>
-					<input type="text" placeholder="Your Name" className={styles.inputField} />
-					<input type="email" placeholder="Your Email" className={styles.inputField} />
-					<input type="text" placeholder="Your Phone" className={styles.inputField} />
+					<input type="text" placeholder="Your Name*"  className={styles.inputField} />
+					<input type="email" placeholder="Your Email*" className={styles.inputField} />
+					<input type="text" placeholder="Your Phone*" className={styles.inputField} />
 					<textarea placeholder="Your Message" className={styles.textArea}></textarea>
 					<button type="submit" className={styles.submitButton}>Send Message</button>
 				</form>
-			</div>
-			<footer />
-		</div>
+			</section>
+		</main>
 	);
 };
 
