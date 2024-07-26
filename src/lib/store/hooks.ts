1a012037891d53daabada27b35947dd474584274
DenisShagi <149366145+DenisShagi@users.noreply.github.com>
2024-07-26 16:59:03 +0500

    refactor: refactor hooks

--- src/lib/store/hooks.ts
+++ src/lib/store/hooks.ts
@@ -1,8 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from "react-redux";
+
 import type { RootState, AppDispatch, AppStore } from "@/store";
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export const useAppStore = () => useStore<AppStore>();
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
